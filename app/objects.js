c30ae3c9d1c0d3707d33072f07fd04e396b87d34
willthefirst <willthefirst@gmail.com>
2012-08-23 11:36:23 -0700

    finished objects and context

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+		return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
-    }
-  };
+		var ownProperties = [];
+		for (var prop in obj) {
+			if (obj.hasOwnProperty(prop)) {
+				ownProperties.push(prop + ': ' + obj[prop]);
+			}
+		}
+		return ownProperties;
+	}
+ };
 });
